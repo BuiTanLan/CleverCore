1a2a1e611de8e4c4e96f76d665b84208206c2aa4
lan <tanlanhcmus@gmail.com>
2021-03-08 23:37:36 +0700

    constructor test

---
+++ CleverCore.Utilities.Test/Dtos/GenericResultTest.cs
@@ -0,0 +1,18 @@
+﻿using CleverCore.Utilities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CleverCore.Utilities.Test.Dtos
+{
+    public class GenericResultTest
+    {
+        [Fact]
+        public void Constructor_CreateObjectNotNull_ValidObjectNoParamDefault()
+        {
+            var genericResult = new GenericResult();
+            Assert.NotNull(genericResult);
+        }
+    }
+}
---
+++ CleverCore.Utilities.Test/Helper/TagHelperTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleverCore.Utilities.Test.Helper
+{
+    class TagHelperTest
+    {
+    }
+}
